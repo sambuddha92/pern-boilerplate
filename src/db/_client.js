2a43999d4032e4d79e27f1098261f99e13b9b114
sambuddha92 <sambuddhaadhikari@gmail.com>
2020-11-04 09:46:16 +0530

    herokuconfig

--- src/db/_client.js
+++ src/db/_client.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
-const Client= require('pg').Client;
-
-const client = new Client({
+const Client = require("pg").Client;
+const devConfig = {
   user: process.env.PGDB_USER,
   host: process.env.PGDB_HOST,
   database: process.env.PGDB_NAME,
   password: process.env.PGDB_PASSWORD,
   port: process.env.PGDB_PORT,
-});
+};
+
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+const client = new Client(
+  process.env.NODE_ENV === "production" ? prodConfig : devConfig
+);
 
 module.exports = client;
--- src/db/_pool.js
+++ src/db/_pool.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 
-const pool = new Pool({
-    user: process.env.PGDB_USER,
-    host: process.env.PGDB_HOST,
-    database: process.env.PGDB_NAME,
-    password: process.env.PGDB_PASSWORD,
-    port: process.env.PGDB_PORT,
-  });
+const devConfig = {
+  user: process.env.PGDB_USER,
+  host: process.env.PGDB_HOST,
+  database: process.env.PGDB_NAME,
+  password: process.env.PGDB_PASSWORD,
+  port: process.env.PGDB_PORT,
+};
+
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? prodConfig : devConfig
+);
 
 module.exports = pool;
